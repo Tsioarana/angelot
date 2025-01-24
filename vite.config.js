3361e54e58f2b881b9679f3122330cb14d6aa543
tsiferana Angelot <131231878+Tsioarana@users.noreply.github.com>
2025-01-24 16:26:07 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
   },
+  build: {
+    rollupOptions: {
+      output: {
+        // Séparation des modules en chunks plus petits
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Crée un chunk séparé pour chaque dépendance dans node_modules
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+    // Augmente la limite d'avertissement de taille des chunks à 2000 kB (ou plus selon vos besoins)
+    chunkSizeWarningLimit: 2000,
+  },
 })
+
