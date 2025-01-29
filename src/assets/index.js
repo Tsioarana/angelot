e580bbe98c30e98fd583689574f749431f86144d
tsiferana Angelot <131231878+Tsioarana@users.noreply.github.com>
2025-01-29 11:11:33 +0300

    Update web

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,5 @@
 import logo from "./logo.png";
 import backend from "./backend.png";
-import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
@@ -53,7 +52,6 @@ export {
   prestashop,
   logo,
   backend,
-  web,
   github,
   menu,
   close,
