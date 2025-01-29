b27822aac174883f8bebf7d9da28e89db84b89a7
tsiferana Angelot <131231878+Tsioarana@users.noreply.github.com>
2025-01-29 11:14:17 +0300

    Update carrent

--- src/assets/index.js
+++ src/assets/index.js
@@ -22,16 +22,6 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import diplome from "./company/diplome.png"
-import shopify from "./company/shopify.png";
-import formation from "./company/formation.png"
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 import projet1 from "./projet1.png";
 import projet2 from "./projet2.png";
 import projet3 from "./projet3.png";
@@ -68,15 +58,6 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  diplome,
-  formation,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
   projet1,
   projet2,
   projet3,
